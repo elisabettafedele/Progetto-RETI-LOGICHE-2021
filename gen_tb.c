ad05c309d8644d21d1033abbf84925abde7bf31b
poppi99 <79159833+poppi99@users.noreply.github.com>
2021-02-18 23:35:07 +0100

    Add files via upload

--- gen_tb.c
+++ gen_tb.c
@@ -10,6 +10,7 @@ int log2 (int n){
 	}
 	return 0;
 }
+
 int shift_f (int a, int shift){
 	int i,n;
 	n=a;
@@ -38,12 +39,13 @@ int main(){
 	srand((unsigned) time(&t));
 	
 	FILE* fp = fopen ("tb.txt","w"); // cambia in .txt se vuoi leggere i risultati !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	int i,j,pixel,schifo,delta,shift,temp,k;
 	int clock;
 	printf("Scegli il clock in nanosecondi\n");
 	scanf("%d",&clock);
 	
 // ARCHITECTURE
-	fprintf(fp,"\nlibrary ieee;\nuse ieee.std_logic_1164.all;\nuse ieee.numeric_std.all;\n\n\nuse ieee.std_logic_unsigned.all;\n");
+	fprintf(fp,"\nlibrary ieee;\nuse ieee.std_logic_1164.all;\nuse ieee.numeric_std.all;\nuse ieee.std_logic_unsigned.all;\n\n\n");
 	fprintf(fp,"entity project_tb is\nend project_tb;\n\n\narchitecture projecttb of project_tb is\nconstant c_CLOCK_PERIOD: time := %d ns;\n",clock);
 	fprintf(fp,"signal   tb_done                : std_logic;\n");
 	fprintf(fp,"signal   mem_address            : std_logic_vector (15 downto 0) := (others => '0');\n");
@@ -52,20 +54,25 @@ int main(){
 	fprintf(fp,"signal   tb_clk                 : std_logic := '0';\n");
 	fprintf(fp,"signal   mem_o_data,mem_i_data  : std_logic_vector (7 downto 0);\n");
 	fprintf(fp,"signal   enable_wire            : std_logic;\n");
-	fprintf(fp,"signal   mem_we                 : std_logic;\n");
-	fprintf(fp,"type ram_type is array (65535 downto 0) of std_logic_vector(7 downto 0);\n");
-	fprintf(fp,"signal RAM: ram_type := ( ");
+	fprintf(fp,"signal   mem_we                 : std_logic;\n\n");
+	fprintf(fp,"type ram_type is array (65535 downto 0) of std_logic_vector(7 downto 0);\n\n");
 //RAM	
 	int n_immagini;
 	printf("Inserisci numero immagini\n");
 	scanf("%d",&n_immagini);
-	int i,j,pixel,schifo,delta,shift,temp;
+	if(n_immagini>1){
+	fprintf(fp,"signal i: std_logic_vector(%d downto 0) := \"",n_immagini-1);
+	for(i=0;i<n_immagini;i++)
+	fprintf(fp,"0");
+	fprintf(fp,"\";\n");
+	}
+	fprintf(fp,"\n\n");
+	
 	int* immagini[n_immagini];
 	int* img_equalizzate[n_immagini];
-	int row[n_immagini];
-	int min[n_immagini];
-	int max[n_immagini];
-	int col[n_immagini];
+	int row[n_immagini],col[n_immagini];
+	int min[n_immagini],max[n_immagini];
+	
 	for (i=0; i<n_immagini;i++){
 		min[i]=255;
 		max[i]=0;
@@ -95,8 +102,9 @@ int main(){
 			img_equalizzate[i][j]=temp;
 		}
 	}
-	
-	int k=0; // indice della memoria
+	if(n_immagini ==1){
+			k=0;
+		fprintf(fp,"signal RAM: ram_type := ( ");
 		pixel = row[0]*col[0];
 		fprintf(fp,"%d => std_logic_vector(to_unsigned(  %d  , 8)),\n",k,col[0]);
 		k++;
@@ -106,8 +114,24 @@ int main(){
 			fprintf(fp,"%d => std_logic_vector(to_unsigned(  %d  , 8)),\n",k,immagini[0][j]);
 			k++;
 		}
-		k=k+pixel+1;
-	fprintf(fp,"others => (others =>'0'));\n\n\n");
+	fprintf(fp,"others => (others =>'0'));\n\n");
+	}
+	else{
+	for(i=0;i<n_immagini;i++){
+		k=0;
+		fprintf(fp,"signal RAM%d: ram_type := ( ",i+1);
+		pixel = row[i]*col[i];
+		fprintf(fp,"%d => std_logic_vector(to_unsigned(  %d  , 8)),\n",k,col[i]);
+		k++;
+		fprintf(fp,"%d => std_logic_vector(to_unsigned(  %d  , 8)),\n",k,row[i]);
+		k++;
+		for(j=0;j<pixel;j++){
+			fprintf(fp,"%d => std_logic_vector(to_unsigned(  %d  , 8)),\n",k,immagini[i][j]);
+			k++;
+		}
+	fprintf(fp,"others => (others =>'0'));\n\n");
+}
+}
 //COMPONENT
 	fprintf(fp,"component project_reti_logiche is\n");
 	fprintf(fp,"port (\n");
@@ -135,7 +159,7 @@ int main(){
 	fprintf(fp,"o_en   	=> enable_wire,\n");
 	fprintf(fp,"o_we 		=> mem_we,\n");
 	fprintf(fp,"o_data    	=> mem_i_data\n");
-	fprintf(fp,");\n\n\n");
+	fprintf(fp,");\n\n");
 //P CLK GEN PROCESS
     fprintf(fp,"p_CLK_GEN : process is\n");
 	fprintf(fp,"begin\n");
@@ -147,20 +171,43 @@ int main(){
     fprintf(fp,"begin\n");
 	fprintf(fp,"if tb_clk'event and tb_clk = '1' then\n");
 	fprintf(fp,"if enable_wire = '1' then\n");
+	if(n_immagini==1){
 	fprintf(fp,"if mem_we = '1' then\n");
     fprintf(fp,"RAM(conv_integer(mem_address))  <= mem_i_data;\n");
     fprintf(fp,"mem_o_data                      <= mem_i_data after 1 ns;\n");
 	fprintf(fp,"else\n");
 	fprintf(fp,"mem_o_data <= RAM(conv_integer(mem_address)) after 1 ns;\n");
+	}
+	else{
+		for(i=0;i<n_immagini;i++){
+			if(i==0)
+				fprintf(fp,"if i = \"");
+			else
+				fprintf(fp,"elsif i = \"");
+			for(j=0;j<n_immagini;j++){
+				if(j == n_immagini-i-1)
+					fprintf(fp,"1");
+				else
+					fprintf(fp,"0");
+			}
+			fprintf(fp,"\" then\n");
+			fprintf(fp,"if mem_we = '1' then\n");
+    		fprintf(fp,"RAM%d(conv_integer(mem_address))  <= mem_i_data;\n",i+1);
+    		fprintf(fp,"mem_o_data                      <= mem_i_data after 1 ns;\n");
+			fprintf(fp,"else\n");
+			fprintf(fp,"mem_o_data <= RAM%d(conv_integer(mem_address)) after 1 ns;\n",i+1);
+			fprintf(fp,"end if;\n");
+		}
+	}
 	fprintf(fp,"end if;\n");
 	fprintf(fp,"end if;\n");
 	fprintf(fp,"end if;\n");
     fprintf(fp,"end process;\n\n\n");
 // TEST PROCESS
-    fprintf(fp,"test : process is\n");
+	fprintf(fp,"test : process is\n");
     fprintf(fp,"begin \n");
+	if(n_immagini == 1){   
 	fprintf(fp,"wait for 100 ns;\n");
-    for(i=0;i<n_immagini;i++){
 	fprintf(fp,"wait for c_CLOCK_PERIOD;\n");
 	fprintf(fp,"tb_rst <= '1';\n");
     fprintf(fp,"wait for c_CLOCK_PERIOD;\n");
@@ -176,27 +223,65 @@ int main(){
 	fprintf(fp,"wait until tb_done = '0';\n");
 	fprintf(fp,"wait for 100 ns;\n");
 	}
-
-int n_assert=0;
-for(i=0;i<n_immagini;i++)
-n_assert=n_assert+(row[i]*col[i]);
-
-k=2;
-for(i=0;i<n_immagini;i++){
-	k=k+(row[i]*col[i]);
-	
-	for(j=0;j<row[i]*col[i];j++){
-	fprintf(fp,"assert RAM(%d) = std_logic_vector(to_unsigned( %d , 8)) report \"TEST FALLITO (WORKING ZONE). Expected  %d  found\"",k,img_equalizzate[i][j],img_equalizzate[i][j]);
-	fprintf(fp," & integer'image(to_integer(unsigned(RAM(%d))))  severity failure;\n",k);
-	k++;
+	else{
+		for(i=0;i<n_immagini;i++){			
+			fprintf(fp,"i <= \"");
+				for(j=0;j<n_immagini;j++){
+				if(j == n_immagini-i-1)
+					fprintf(fp,"1");
+				else
+					fprintf(fp,"0");
+			}
+			fprintf(fp,"\";\n");
+			if(i!=0) fprintf(fp, "\n\n");
+			else{
+				fprintf(fp,"wait for 100 ns;\n");
+				fprintf(fp,"wait for c_CLOCK_PERIOD;\n");
+				fprintf(fp,"tb_rst <= '1';\n");
+    			fprintf(fp,"wait for c_CLOCK_PERIOD;\n");
+    			fprintf(fp,"wait for 100 ns;\n");
+				fprintf(fp,"tb_rst <= '0';\n");
+				fprintf(fp,"wait for c_CLOCK_PERIOD;\n");
+			}
+			fprintf(fp,"wait for 100 ns;\n");
+			fprintf(fp,"tb_start <= '1';\n");
+    		fprintf(fp,"wait for c_CLOCK_PERIOD;\n");
+    		fprintf(fp,"wait until tb_done = '1';\n");
+    		fprintf(fp,"wait for c_CLOCK_PERIOD;\n");
+			fprintf(fp,"tb_start <= '0';\n");
+			fprintf(fp,"wait until tb_done = '0';\n");
+			fprintf(fp,"wait for 100 ns;\n");
+	}}
+fprintf(fp,"\n");
+if(n_immagini == 1){
+pixel=row[0]*col[0];
+k=2+pixel;
+for(j=0;j<pixel;j++){
+fprintf(fp,"assert RAM(%d) = std_logic_vector(to_unsigned( %d , 8)) report \"TEST FALLITO (WORKING ZONE). Expected  %d  found\"",k,img_equalizzate[0][j],img_equalizzate[0][j]);
+fprintf(fp," & integer'image(to_integer(unsigned(RAM(%d))))  severity failure;\n",k);
+k++;
 }
-	k=k+2;
+fprintf(fp,"\n\n\n");
+}
+else{
+for(i=0;i<n_immagini;i++){
+pixel=row[i]*col[i];
+k=2+pixel;
+for(j=0;j<pixel;j++){
+fprintf(fp,"assert RAM%d(%d) = std_logic_vector(to_unsigned( %d , 8)) report \"TEST FALLITO (WORKING ZONE). Expected  %d  found\"",i+1,k,img_equalizzate[i][j],img_equalizzate[i][j]);
+fprintf(fp," & integer'image(to_integer(unsigned(RAM%d(%d))))  severity failure;\n",i+1,k);
+k++;
+}
+fprintf(fp,"\n");
+if(i==n_immagini-1)
+	fprintf(fp,"\n\n");
+}
 }
 
     fprintf(fp,"assert false report \"Simulation Ended! TEST PASSATO\" severity failure;\n");
     fprintf(fp,"end process test;\n");
 	fprintf(fp,"end projecttb;\n");
-stampa(n_immagini,immagini,img_equalizzate,row,col);
+	stampa(n_immagini,immagini,img_equalizzate,row,col);
 	
 	fclose(fp);
 	return 0;
